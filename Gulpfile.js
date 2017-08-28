911d89508a47ce25ef26125f86ff45d7f44b0bdd
Dale Sande <dale.sande@gmail.com>
2017-08-28 08:38:34 -0700

    rearchitect dir structure for dist build process

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,7 +6,16 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
-    reload = browserSync.reload;
+    reload = browserSync.reload,
+    clean = require('gulp-clean');
+
+// var listing of files for dist build
+var filesToDist = [
+    './src/*.html',
+    './src/css/**/*.*',
+    './src/images/**/*.*',
+    './src/js//**/*.js'
+];
 
 // Use for stand-alone autoprefixer
 var gulpautoprefixer = require('gulp-autoprefixer');
@@ -17,7 +26,7 @@ var postcss = require('gulp-postcss'),
 
 // Gulp task when using gulp-autoprefixer as a standalone process
 gulp.task('build:css', function() {
-    gulp.src('./sass/{,*/}*.{scss,sass}')
+    gulp.src('./src/sass/{,*/}*.{scss,sass}')
         .pipe(sourcemaps.init())
         .pipe(sass({
         errLogToConsole: true,
@@ -28,7 +37,7 @@ gulp.task('build:css', function() {
         cascade: false
     }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./src/css'))
         .pipe(reload({stream: true}));
 });
 
@@ -36,17 +45,32 @@ gulp.task('build:css', function() {
 gulp.task('serve', ['build:css'], function() {
 
     browserSync.init({
-        server: "./",
+        server: "./src/",
         port: 8080
     });
 
-    gulp.watch('./sass/{,*/}*.{scss,sass}', ['build:css']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch('./src/sass/{,*/}*.{scss,sass}', ['build:css']);
+    gulp.watch("./src/*.html").on('change', browserSync.reload);
 });
 
 // Sass watcher
 gulp.task('sass:watch', function() {
-    gulp.watch('./sass/{,*/}*.{scss,sass}', ['build:css'])
-})
+    gulp.watch('./src/sass/{,*/}*.{scss,sass}', ['build:css'])
+});
+
+// resource cleaning task
+gulp.task('clean', function(){
+  return gulp.src(['dist/*'], {read:false})
+  .pipe(clean());
+});
+
+// dist build tasks
+// see var filesToDist for specific files
+gulp.task('build:dist',['clean'], function(){
+  // the base option sets the relative root for the set of files,
+  // preserving the folder structure
+  gulp.src(filesToDist, { base: './src/' })
+  .pipe(gulp.dest('dist'));
+});
 
 gulp.task('default', ['build:css', 'sass:watch', 'serve']);
--- README.md
+++ README.md
@@ -14,11 +14,13 @@ $ bower install html5-boilerplate-sass-gulp
 Default task is `$ gulp`, which will run all the following commands. Or, you can use them individually as needed.
 
 ```
-$ gulp build:css
+$ gulp build:css // only processes Sass to CSS once
 
-$ gulp sass:watch
+$ gulp sass:watch // run Sass watcher for updated files
 
-$ gulp serve
+$ gulp serve // starts browser-sync server
+
+$ gulp build:dist // moves processed files from src/ to dist/
 ```
 
 ### browserSync support
